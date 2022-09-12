b5dc491f3dc048dc7176227bac0d54cf0850cadb
U-BSD\455152 <455152@SHS-DGCTCS2.beaverton.k12.or.us>
2022-09-12 13:15:52 -0700

    Guessing Game Assignment

---
+++ Guessing Game/guess.cpp
@@ -0,0 +1,52 @@
+/*
+This is a very simple guessing game.  The program generates a random number and you, the user, has to try to guess it in as little tries as possible.
+
+Author: Morgan Hinz
+Creation Date: 8 September 2022
+
+Made for Mr. Galbraith's C++ class.
+
+Rules of C++ for Mr. Galbraith:
+  1. No global variables.
+  2. Strings are NEVER allowed.  Cstrings and character arrays are fine.
+  3. Include <iostream> and not stdio.
+  4. Avoid using a mouse when possible.
+ */
+
+#include <iostream> // required to print to the console.
+
+using namespace std; // standard library
+
+/*
+  The main function of the program.
+ */
+int main() {
+  srand(time(NULL)); // Sets the random seed.
+
+  int randomNumber = rand() % 100 + 1; // Sets the number that the user has to guess.
+
+  int guesses = 0; // Keeps track of the guesses made.
+
+  cout << "Type a number: ";
+  int guess; // Allocates an int for the user's guess. (changes over time)
+  cin >> guess; // Read user input
+  cout << endl;
+
+  do { // Keep guessing until the guessed number is equal to the random chosen number.
+    cin.ignore(1, '\n'); // Ignores the newline character
+    ++guesses; // Increase guesses
+    if (guess > randomNumber) {
+      cout << "Your guess is greater than my number! (Guesses: " << guesses << ") Guess again." << endl;
+      cin >> guess;
+    } else if (guess < randomNumber) {
+      cout << "Your guess is less than my number! (Guesses: " << guesses << ") Guess again." << endl;
+      cin >> guess;
+    }
+  } while (guess != randomNumber);
+
+  ++guesses;
+  
+  cout << "You guessed my number correctly!  It was " << randomNumber << ".  You guessed my number in " << guesses << " guesses." << endl;
+  
+  return 0;
+}
