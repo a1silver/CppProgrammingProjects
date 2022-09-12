79c3b7410d37b299309a5d12d6336d548fb843fe
U-BSD\455152 <455152@SHS-DGCTCS2.beaverton.k12.or.us>
2022-09-12 13:39:08 -0700

    Guessing Game Assignment

--- Guessing Game/guess.cpp
+++ Guessing Game/guess.cpp
@@ -23,30 +23,44 @@ using namespace std; // standard library
 int main() {
   srand(time(NULL)); // Sets the random seed.
 
-  int randomNumber = rand() % 100 + 1; // Sets the number that the user has to guess.
+  bool playing = true;
 
-  int guesses = 0; // Keeps track of the guesses made.
+  while(playing) {
+    int randomNumber = rand() % 100 + 1; // Sets the number that the user has to guess.
 
-  cout << "Type a number: ";
-  int guess; // Allocates an int for the user's guess. (changes over time)
-  cin >> guess; // Read user input
-  cout << endl;
+    int guesses = 0; // Keeps track of the guesses made.
 
-  do { // Keep guessing until the guessed number is equal to the random chosen number.
-    cin.ignore(1, '\n'); // Ignores the newline character
-    ++guesses; // Increase guesses
-    if (guess > randomNumber) {
-      cout << "Your guess is greater than my number! (Guesses: " << guesses << ") Guess again." << endl;
-      cin >> guess;
-    } else if (guess < randomNumber) {
-      cout << "Your guess is less than my number! (Guesses: " << guesses << ") Guess again." << endl;
-      cin >> guess;
-    }
-  } while (guess != randomNumber);
+    cout << "Type a number: ";
+    int guess; // Allocates an int for the user's guess. (changes over time)
+    cin >> guess; // Read user input
+    cout << endl;
 
-  ++guesses;
+    do { // Keep guessing until the guessed number is equal to the random chosen number.
+      cin.ignore(1, '\n'); // Ignores the newline character
+      ++guesses; // Increase guesses
+      if (guess > randomNumber) {
+	cout << "Your guess is greater than my number! (Guesses: " << guesses << ") Guess again." << endl;
+	cin >> guess;
+      } else if (guess < randomNumber) {
+	cout << "Your guess is less than my number! (Guesses: " << guesses << ") Guess again." << endl;
+	cin >> guess;
+      }
+    } while (guess != randomNumber);
+
+    ++guesses;
   
-  cout << "You guessed my number correctly!  It was " << randomNumber << ".  You guessed my number in " << guesses << " guesses." << endl;
+    cout << "You guessed my number correctly!  It was " << randomNumber << ".  You guessed my number in " << guesses << " guesses." << endl;
+    cout << "Would you like to play again? (Y/N)" << endl;
+    char ch;
+    do {
+      cin >> ch;
+    } while (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n');
+
+    if(ch == 'N' || ch == 'n') {
+      playing = false;
+    }
+  
+  }
   
   return 0;
 }
