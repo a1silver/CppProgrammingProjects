7aa9c3063c7f1bc305b15c8325bbfbab5f1629f9
U-BSD\455152 <455152@SHS-DGCTCS2.beaverton.k12.or.us>
2022-09-14 14:29:03 -0700

    Palindrome Updates

--- Palindrome/palindrome.cpp
+++ Palindrome/palindrome.cpp
@@ -1,23 +1,73 @@
 #include <iostream>
 #include <cstring>
+#include <ctype.h>
 
 using namespace std;
 
 int main() {
-  cout << "This is a very simple palindrome tester!  Please input up to 80 characters:" << endl;
-  char rawinput[80];
-  cin >> ws;
-  getline(cin, rawinput);
-  cin.ignore(1, '\n');
-  
-  char input[80];
-  strcpy(input, rawinput);
-  puts(input);
+  bool testing = true;
 
-  for(int i = 0; i < 80; ++i) {
-    cout << input[i];
-  }
+  cout << "This is a very simple palindrome tester! ";
   
+  while (testing) {
+    cout << "Please input up to 80 characters: ";
+    char rawinput[81]; // allow for a null character to be written if the user inputs 80 characters!
+    cin >> ws; // strip whitespace.
+    cin.getline(rawinput, 80, '\n');
+
+    char strippedinput[81];
+    int isize = 0;
+  
+    for(int i = 0; i < 81; i++) {
+      bool flag1 = isalpha(rawinput[i]);
+      bool flag2 = rawinput[i] == '\0';
+      if(flag1 || flag2) {
+	strippedinput[isize] = rawinput[i];
+	isize++;
+      }
+      if(flag2) {
+	break;
+      }
+    }
+    cout << isize << endl;
+    char input[isize];
+    char backwardsinput[isize];
+  
+    for(int i = 0; i < isize; i++) {
+      input[i] = strippedinput[i];
+    }
+    for(int i = isize - 2, j = 0; i >= 0; i--, j++) {
+      backwardsinput[j] = strippedinput[i];
+    }
+    backwardsinput[isize - 1] = '\0';
+
+    for(int i = 0; i < strlen(input); i++) {
+      cout << input[i];
+    }
+    cout << endl;
+    for(int i = 0; i < strlen(backwardsinput); i++) {
+      cout << backwardsinput[i];
+    }
+    cout << endl;
+
+    int compareResult = strcmp(input, backwardsinput);
+
+    if(compareResult == 0) {
+      cout << "That phrase is a palindrome!" << endl;
+    } else {
+      cout << "That phrase is not a palindrome!" << endl;
+    }
+
+    cout << "Would you like to test another palindrome? (Y/N)" << endl;
+    char ch;
+    do {
+      cin >> ch;
+    } while (ch != 'Y' && ch != 'y' && ch != 'N' && ch != 'n');
+
+    if(ch == 'N' || ch == 'n') {
+      testing = false; // This will break out of the loop and terminate the application.
+    }
+  }
   
   return 0;
 }
