35c31d08091235ff149062b74f3d330a9e82605d
Morgan Hinz <455152@bsd48.org>
2023-01-09 21:43:18 -0800

    Added LinkedList class comments

--- LinkedListsPart2/linkedlist.h
+++ LinkedListsPart2/linkedlist.h
@@ -33,3 +33,8 @@ public:
 };
 
 #endif // LINKEDLIST_H
+/*
+This is the LinkedList class!
+This class helps clean up operations done on a linked list.  It contains vital functions like sorting, appending, deleting, GPA averaging, and more.
+Some of the utility functions like getPrevious(), swap(), bubbleSort(), and printNode() are private because they're used only by other public functions.
+*/
